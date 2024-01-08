57a91fbbdf2c740a51cec0de565827d2a62306f5
José Muñoz <jdmg93@gmail.com>
2024-01-07 22:40:21 -0800

    refactor: adds typescript, and replaces gulp with rollup, chai with jest

--- dist/cjs/Agent.js
+++ dist/cjs/Agent.js
@@ -1,15 +1,28 @@
-/** 
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const defaultAgentOptions = {
+    expiryTime: 10,
+    toleranceWndInner: 0.04,
+    toleranceWndPre: 0.15,
+    toleranceWndPost: 0.3,
+    correctionFactor: 50,
+    maxChange: 0.2,
+    penaltyFactor: 0.5,
+};
+/**
  * Agent is the central class for beat tracking
  * @class
  */
-export default class Agent {
+class Agent {
     /**
      * Constructor
      * @param {Number} tempo - tempo hypothesis of the Agent
      * @param {Number} firstBeatTime - the time of the first beat accepted by this Agent
      * @param {Number} firsteventScore - salience value of the first beat accepted by this Agent
-     * @param {Array} agentList - reference to the agent list 
-     * @param {Object} [params={}] - parameters     
+     * @param {Array} agentList - reference to the agent list
+     * @param {Object} [params={}] - parameters
      * @param {Number} [params.expiryTime=10] - the time after which an Agent that has not accepted any beat will be destroyed
      * @param {Number} [params.toleranceWndInner=0.04] - the maximum time that a beat can deviate from the predicted beat time without a fork occurring
      * @param {Number} [params.toleranceWndPre=0.15] - the maximum amount by which a beat can be earlier than the predicted beat time, expressed as a fraction of the beat period
@@ -17,82 +30,79 @@ export default class Agent {
      * @param {Number} [params.correctionFactor=50] - correction factor for updating beat period
      * @param {Number} [params.maxChange=0.2] - the maximum allowed deviation from the initial tempo, expressed as a fraction of the initial beat period
      * @param {Number} [params.penaltyFactor=0.5] - factor for correcting score, if onset do not coincide precisely with predicted beat time
-     */      
-    constructor(tempo, firstBeatTime, firsteventScore, agentList, params = {}) {
-        /** 
+     */
+    constructor(tempo, firstBeatTime, firsteventScore, agentList, params = defaultAgentOptions) {
+        /**
          * the time after which an Agent that has not accepted any beat will be destroyed
-         * @type {Number} 
+         * @type {Number}
          */
         this.expiryTime = params.expiryTime || 10;
-        /** 
+        /**
          * the maximum time that a beat can deviate from the predicted beat time without a fork occurring
-         * @type {Number} 
+         * @type {Number}
          */
         this.toleranceWndInner = params.toleranceWndInner || 0.04;
-        /** 
+        /**
          * the maximum amount by which a beat can be earlier than the predicted beat time, expressed as a fraction of the beat period
-         * @type {Number} 
+         * @type {Number}
          */
         this.toleranceWndPre = params.toleranceWndPre || 0.15;
-        /** 
+        /**
          * the maximum amount by which a beat can be later than the predicted beat time, expressed as a fraction of the beat period
-         * @type {Number} 
+         * @type {Number}
          */
         this.toleranceWndPost = params.toleranceWndPost || 0.3;
-
-        this.toleranceWndPre *= tempo;
-        this.toleranceWndPost *= tempo;
-
-        /** 
+        this.toleranceWndPre *= tempo || 0;
+        this.toleranceWndPost *= tempo || 0;
+        /**
          * correction factor for updating beat period
-         * @type {Number} 
+         * @type {Number}
          */
         this.correctionFactor = params.correctionFactor || 50;
-        /** 
+        /**
          * the maximum allowed deviation from the initial tempo, expressed as a fraction of the initial beat period
-         * @type {Number} 
-         */        
+         * @type {Number}
+         */
         this.maxChange = params.maxChange || 0.2;
-        /** 
+        /**
          * factor for correcting score, if onset do not coincide precisely with predicted beat time
-         * @type {Number} 
-         */        
+         * @type {Number}
+         */
         this.penaltyFactor = params.penaltyFactor || 0.5;
-
-        /** 
+        /**
          * the current tempo hypothesis of the Agent, expressed as the beat period
-         * @type {Number} 
-         */ 
+         * @type {Number}
+         */
         this.beatInterval = tempo;
-        /** 
+        /**
          * the initial tempo hypothesis of the Agent, expressed as the beat period
          * @type {Number}
-         */         
+         */
         this.initialBeatInterval = tempo;
-        /** 
+        /**
          * the time of the most recent beat accepted by this Agent
-         * @type {Number} 
-         */         
-        this.beatTime = firstBeatTime;
-        /** 
+         * @type {Number}
+         */
+        this.beatTime = firstBeatTime || 0;
+        /**
          * the number of beats found by this Agent, including interpolated beats
-         * @type {Number} 
-         */         
+         * @type {Number}
+         */
         this.totalBeatCount = 1;
-        /** 
+        /**
          * the array of onsets accepted by this Agent as beats, plus interpolated beats
-         * @type {Array} 
-         */         
-        this.events = [firstBeatTime];
-        /** 
+         * @type {Array}
+         */
+        this.events = [firstBeatTime || 0];
+        /**
          * sum of salience values of the onsets which have been interpreted as beats by this Agent
-         * @type {Number} 
-         */         
+         * @type {Number}
+         */
         this.score = firsteventScore;
-        /** 
-         * reference to the agent list 
-         * @type {Array} 
-         */         
+        /**
+         * reference to the agent list
+         * @type {Array}
+         */
         this.agentListRef = agentList;
     }
     /**
@@ -102,17 +112,16 @@ export default class Agent {
      * @return {Boolean} indicate whether the given event time was accepted as a beat time
      */
     considerEvent(eventTime, eventScore) {
+        var _a;
         if (eventTime - this.events[this.events.length - 1] > this.expiryTime) {
             this.score = -1;
             return false;
         }
-
-        let beatCount = Math.round( (eventTime - this.beatTime) / this.beatInterval );
+        let beatCount = Math.round((eventTime - this.beatTime) / this.beatInterval);
         let err = eventTime - this.beatTime - beatCount * this.beatInterval;
-
         if (beatCount > 0 && err >= -this.toleranceWndPre && err <= this.toleranceWndPost) {
             if (Math.abs(err) > this.toleranceWndInner) {
-                this.agentListRef.push(this.clone());
+                (_a = this.agentListRef) === null || _a === void 0 ? void 0 : _a.push(this.clone());
             }
             this.acceptEvent(eventTime, eventScore, err, beatCount);
             return true;
@@ -125,47 +134,45 @@ export default class Agent {
      * @param {Number} eventScore - salience values of the event time
      * @param {Number} err - the difference between the predicted and actual beat times
      * @param {Number} beatCount - the number of beats since the last beat
-     */    
+     */
     acceptEvent(eventTime, eventScore, err, beatCount) {
         this.beatTime = eventTime;
         this.events.push(eventTime);
-
         let corrErr = err / this.correctionFactor;
         if (Math.abs(this.initialBeatInterval - this.beatInterval - corrErr) < this.maxChange * this.initialBeatInterval) {
             this.beatInterval += corrErr;
         }
         this.totalBeatCount += beatCount;
-        let errFactor =  err > 0 ? err / this.toleranceWndPost : err / -this.toleranceWndPre;
+        let errFactor = err > 0 ? err / this.toleranceWndPost : err / -this.toleranceWndPre;
         let scoreFactor = 1 - this.penaltyFactor * errFactor;
         this.score += eventScore * scoreFactor;
     }
     /**
      * Interpolates missing beats in the Agent's beat track
-     */     
+     */
     fillBeats() {
         let prevBeat, nextBeat, currentInterval, beats;
         prevBeat = 0;
         if (this.events.length > 2) {
             prevBeat = this.events[0];
         }
-
         for (let i = 0; i < this.events.length; i++) {
             nextBeat = this.events[i];
             beats = Math.round((nextBeat - prevBeat) / this.beatInterval - 0.01);
             currentInterval = (nextBeat - prevBeat) / beats;
             let k = 0;
-            for ( ; beats > 1; beats--) {
+            for (; beats > 1; beats--) {
                 prevBeat += currentInterval;
                 this.events.splice(i + k, 0, prevBeat);
                 k++;
             }
-            prevBeat = nextBeat;            
+            prevBeat = nextBeat;
         }
     }
     /**
      * Makes a clone of the Agent
      * @return {Agent} agent's clone
-     */     
+     */
     clone() {
         let newAgent = new Agent();
         newAgent.beatInterval = this.beatInterval;
@@ -182,7 +189,8 @@ export default class Agent {
         newAgent.penaltyFactor = this.penaltyFactor;
         newAgent.score = this.score;
         newAgent.agentListRef = this.agentListRef;
-
         return newAgent;
     }
 }
+
+exports.default = Agent;
